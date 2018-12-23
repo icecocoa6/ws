af780f3401f5a5216b8d94a96b4d7edda2b27daf
Taichi Tomioka <mys.itachi@gmail.com>
2018-12-23 18:36:27 +0900

    Add schemes.

--- src/index.ts
+++ src/index.ts
@@ -100,6 +100,36 @@ async function updateOrCreateWaterServer(entity) {
   }
 }
 
+async function searchMakerDetails(condition) {
+  var result = [];
+  var nextToken;
+  do {
+    const makers = await API.graphql(graphqlOperation(queries.listMakerDetails, {filter: condition, nextToken: nextToken}));
+    nextToken = makers["data"]["listMakerDetails"]["nextToken"];
+    Array.prototype.push.apply(result, makers["data"]["listMakerDetails"]["items"]);
+  } while (nextToken);
+  return result;
+}
+
+async function updateOrCreateMakerDetails(entity) {
+  const searchResult = await searchMakerDetails({maker_id: {eq: entity["maker_id"]}});
+  console.log('searched: ', JSON.stringify(searchResult));
+  if (searchResult.length > 1) return;
+
+  console.log('entity: ', JSON.stringify(entity));
+  if (searchResult.length == 0) {
+    console.log('here');
+    const details = await API.graphql(graphqlOperation(mutations.createMakerDetails, {input: entity}));
+    console.log('create: ', JSON.stringify(details));
+    await API.graphql(graphqlOperation(mutations.updateMaker, {input: {id: entity["maker_id"], maker_details_id: details["data"]["createMakerDetails"]["id"]}}))
+  } else {
+    entity['id'] = searchResult[0]['id'];
+    const details = await API.graphql(graphqlOperation(mutations.updateMakerDetails, {input: entity}));
+    console.log('update: ',JSON.stringify(details));
+    await API.graphql(graphqlOperation(mutations.updateMaker, {input: {id: entity["maker_id"], maker_details_id: details["data"]["updateMakerDetails"]["id"]}}))
+  }
+}
+
 // deleteAllMakers().catch(function (e) { console.log(e); });
 
 function updateMakersFromSheet(sheet) {
@@ -140,9 +170,43 @@ function updateWaterServersFromSheet(maker_id_by_name, sheet) {
     });
 }
 
+function updateMakerDetailsFromSheet(maker_id_by_name, sheet) {
+  return new Promise<void[]>((resolve, reject) => {
+    sheet.getRows((err, rows) => {
+      var promises: Promise<void>[] = [];
+      for (var j in rows) {
+        if (!rows[j]["メーカー名"]) continue;
+        promises.push(
+          updateOrCreateMakerDetails({
+            maker_id: maker_id_by_name[rows[j]["メーカー名"]],
+            monthly_charge: parseInt(rows[j]["月額料金"].replace(/[^0-9]/g, ""), 10),
+            water_price: parseInt(rows[j]["水の注文価格"].replace(/[^0-9]/g, ""), 10),
+            minimum_lot: rows[j]["最低ロット"],
+            rental: parseInt(rows[j]["レンタル料"].replace(/[^0-9]/g, ""), 10),
+            electricity_bill: parseInt(rows[j]["電気代"].replace(/[^0-9]/g, ""), 10),
+            installation_cost: parseInt(rows[j]["初期費用"].replace(/[^0-9]/g, ""), 10),
+            delivery_cost: parseInt(rows[j]["配送料"].replace(/[^0-9]/g, ""), 10),
+            maintenance_fee: parseInt(rows[j]["メンテナンス料金"].replace(/[^0-9]/g, ""), 10),
+            contract_period: rows[j]["契約期間"],
+            cancellation_charge: parseInt(rows[j]["解約料"].replace(/[^0-9]/g, ""), 10),
+            method_of_payment: rows[j]["支払方法"],
+            delivery_method: rows[j]["方式"],
+            delivery_area: rows[j]["エリア"],
+            delivery_time: rows[j]["曜日時間帯"],
+            delivery_cycle: rows[j]["周期"]
+          })
+        )
+      }
+      Promise.all(promises.map(p => p.catch(e => console.log(e))))
+        .then(resolve)
+        .catch(reject)
+    })
+  });
+}
+
 async function main() {
   const sheet = new GoogleSpreadsheet("151YCqajwP56ZHhPmEYWRe3jK_mu-0FkvOkOrl_Nvb2Y");
-  const credentials = require("../credentials/***.json");
+  const credentials = require("../credentials/waterserver-eeb950016754.json");
 
   const makers = await listAllMakers();
   var maker_id_by_name = {};
@@ -163,9 +227,13 @@ async function main() {
           if (data.worksheets[i].title === "1. サーバー情報") {
             promises.push(updateWaterServersFromSheet(maker_id_by_name, data.worksheets[i]));
           }
+
+          if (data.worksheets[i].title === "ランキング生成") {
+            promises.push(updateMakerDetailsFromSheet(maker_id_by_name, data.worksheets[i]))
+          }
         }
 
-        Promise.all(promises).then(resolve).catch(reject);
+        Promise.all(promises.map(p => p.catch(e => e))).then(resolve).catch(reject);
       });
     });
   });
